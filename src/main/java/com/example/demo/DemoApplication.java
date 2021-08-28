254624d29efd45c5254bbe306ebc14cc6546f3e7
Subhashini <subhashini94@gmail.com>
2021-08-28 23:59:19 +0530

    Update display string

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@GetMapping("/test-docker")
 	public String getData() {
-		return "Sample deployment with my latest changes";
+		return "Sample deployment with latest changes";
 	}
 
 	public static void main(String[] args) {
