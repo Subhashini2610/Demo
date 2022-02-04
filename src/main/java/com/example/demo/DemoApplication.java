7624861811c42dff0314dcbf39941d75f87ce029
Subhashini <subhashini94@gmail.com>
2022-02-04 20:39:31 +0530

    Adding java logs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,10 @@ public class DemoApplication {
 
 	@GetMapping("/test-docker")
 	public String getData() {
+
+		System.out.println("Sample logs 1");
+		System.out.println("Sample logs 2");
+
 		return "Sample deployment with latest changes";
 	}
 
